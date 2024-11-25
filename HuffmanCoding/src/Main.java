9562f03818829b164228506e0580b3bd17562b4a
Omar Abdelmonem <omarmohamed012330@gmail.com>
2024-11-25 18:10:45 +0200

    update the input of Standard Huffman Coding

--- HuffmanCoding/src/Main.java
+++ HuffmanCoding/src/Main.java
@@ -11,31 +11,28 @@ public class Main {
         Decompression decompression = new Decompression();
         Scanner scanner = new Scanner(System.in);
         int choice;
-        do {
-            System.out.println("Menu:");
-            System.out.println("1. Compression");
-            System.out.println("2. Decompression");
-            System.out.println("3. Exit");
-            System.out.print("Enter your choice: ");
+        System.out.println("Menu:");
+        System.out.println("1. Compression");
+        System.out.println("2. Decompression");
+        System.out.println("3. Exit");
+        System.out.print("Enter your choice: ");
 
-            choice = scanner.nextInt();
+        choice = scanner.nextInt();
 
-            switch (choice) {
-                case 1:
-                    readWriteFiles.writeStringToFile(compression.compress(readWriteFiles.readStringFromFile("src\\compress.txt")), "src\\decompress.txt");
-                    break;
-                case 2:
-                    readWriteFiles.writeStringToFile(decompression.decompress("src\\decompress.txt"), "src\\compress.txt");
-                    break;
-                case 3:
-                    System.out.println("Exiting the program.");
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please try again.");
-            }
-
-            System.out.println();
-        } while (choice != 3);
+        switch (choice) {
+            case 1:
+                readWriteFiles.writeStringToFile(compression.compress(readWriteFiles.readStringFromFile("src\\compress.txt")), "src\\decompress.txt");
+                break;
+            case 2:
+                readWriteFiles.writeStringToFile(decompression.decompress("src\\decompress.txt"), "src\\compress.txt");
+                break;
+            case 3:
+                System.out.println("Exiting the program.");
+                break;
+            default:
+                System.out.println("Invalid choice. Please try again.");
+        }
+        System.out.println();
 
         scanner.close();
     }
